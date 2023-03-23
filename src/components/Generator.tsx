42f9b276dd286ab1f092ef9b53bc5e0f321579d5
shenghuaxiong <67672773+shenghuaxiong@users.noreply.github.com>
2023-03-23 22:09:04 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -87,7 +87,7 @@ export default function () {
   }
 
   async function checkCode(value?: string) {
-    var ycode = '123abc'
+    var ycode = '871433'
 
     if (value ?? codeRef.value == ycode) {
       setShown(!shown())
